b22343846e09a2881c2601b38903ad9f5c61ca6c
MICHELLE <muthonimichelle64@gmail.com>
2023-02-16 15:36:36 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints exact code
+ *
+ * Return:0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
