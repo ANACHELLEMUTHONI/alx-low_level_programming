574fdb32830618ce501dc2315d85c6cb33646a2a
MICHELLE <muthonimichelle64@gmail.com>
2023-02-22 15:24:23 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,9 +11,9 @@ int main(void)
 
 	sum = 0;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 1024 ; i++)
 	{
-		if((i % 3 == 0) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum = sum + i;
 		}
