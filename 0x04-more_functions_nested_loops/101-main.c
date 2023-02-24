5c95e144eabf5b2b4bfffcfd8044ba9db7e01cf8
MICHELLE <muthonimichelle64@gmail.com>
2023-02-24 14:33:59 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd>
+
+/**
+ * main - check the code
+ * @n: para 1
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
